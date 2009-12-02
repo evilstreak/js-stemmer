419e0cb75696bca82c2f5136b29afb932331208a
Dominic Baggott <dominic.baggott@gmail.com>
2009-12-02 13:34:36 +0000

    Keep track of failed words to rerun verbosely.

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const fs = require( "fs-base" ),
 
 var voc = fs.rawOpen( "voc.txt" ).readWhole().split( "\n" ).slice( 0, 15000 ),
     out = fs.rawOpen( "output.txt" ).readWhole().split( "\n" ).slice( 0, 15000 ),
-    bad = 0,
+    bad = [],
     i;
 
 for ( i = 0; i < voc.length; i++ ) {
@@ -13,9 +13,19 @@ for ( i = 0; i < voc.length; i++ ) {
       target = out[ i ],
       result = Stemmer.stem( word );
   if ( result !== target ) {
-    ++bad;
-    // print( word, " =>", result, "(expected " + target + ")" );
+    bad.push( {
+      word : word,
+      target : target
+    } );
   }
 }
 
-print( bad, "failures (total of", i, "tests)" );
+print( bad.length, "failures (total of", i, "tests)\n" );
+
+// make it verbose and rerun the failures
+Stemmer.log = function() { print.apply( this, arguments ); }
+
+for ( var b = 0; b < bad.length; b++ ) {
+  var result = Stemmer.stem( bad[ b ].word );
+  print( "Got " + result + ", expected " + bad[ b ].target, "\n" );
+}
